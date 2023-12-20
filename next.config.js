1edaa68e51689f76d445c6df64efbb23a06f9647
Mahdi <mahdi.developer1@gmail.com>
2023-12-20 15:06:05 +0330

    add host url for image fetching

--- next.config.js
+++ next.config.js
@@ -4,10 +4,9 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '8000',
-        pathname: '/media/files/property_cover/**',
+        protocol: 'https',
+        hostname: 'realestate-manager.iran.liara.run',
+        pathname: '/media/**',
       },
     ],
   },
